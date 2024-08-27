3acad6c7525b511e67954e644c16479d0dbc0e7e
Essi <essi.antola@hotmail.com>
2024-08-27 09:52:43 +0300

    initial commit

---
+++ trip_planner/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./components/Hero.jsx";
+import Generator from "./components/Generator.jsx";
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Generator />
+    </div>
+  );
+}
+
+export default App;
---
+++ trip_planner/src/utils/Options.js
@@ -0,0 +1,516 @@
+export const DESTINATIONS = [
+  {
+    name: "Paris, France",
+    activities: [
+      "Visit the Eiffel Tower",
+      "Explore the Louvre Museum",
+      "Stroll along the Seine River",
+      "Enjoy a boat cruise",
+      "Dine in a French bistro",
+    ],
+  },
+  {
+    name: "Tokyo, Japan",
+    activities: [
+      "Visit the Tokyo Tower",
+      "Explore Akihabara",
+      "Stroll through Shibuya Crossing",
+      "Visit Senso-ji Temple",
+      "Enjoy sushi at Tsukiji Market",
+    ],
+  },
+  {
+    name: "New York, USA",
+    activities: [
+      "See a Broadway show",
+      "Visit the Statue of Liberty",
+      "Explore Central Park",
+      "Visit the Metropolitan Museum of Art",
+      "Walk through Times Square",
+    ],
+  },
+  {
+    name: "Rome, Italy",
+    activities: [
+      "Visit the Colosseum",
+      "Explore the Vatican Museums",
+      "Stroll through the Roman Forum",
+      "See the Pantheon",
+      "Enjoy Italian cuisine in Trastevere",
+    ],
+  },
+  {
+    name: "Sydney, Australia",
+    activities: [
+      "Visit the Sydney Opera House",
+      "Walk across the Sydney Harbour Bridge",
+      "Relax at Bondi Beach",
+      "Explore the Royal Botanic Garden",
+      "Discover the Taronga Zoo",
+    ],
+  },
+  {
+    name: "Cape Town, South Africa",
+    activities: [
+      "Take a cable car up Table Mountain",
+      "Visit the V&A Waterfront",
+      "Explore Robben Island",
+      "Relax at Camps Bay Beach",
+      "Discover the Cape Winelands",
+    ],
+  },
+  {
+    name: "Barcelona, Spain",
+    activities: [
+      "Visit La Sagrada Família",
+      "Explore Park Güell",
+      "Stroll along La Rambla",
+      "Discover the Gothic Quarter",
+      "Relax at Barceloneta Beach",
+    ],
+  },
+  {
+    name: "Istanbul, Turkey",
+    activities: [
+      "Visit the Hagia Sophia",
+      "Explore the Grand Bazaar",
+      "See the Blue Mosque",
+      "Take a Bosphorus cruise",
+      "Wander through Topkapi Palace",
+    ],
+  },
+  {
+    name: "Rio de Janeiro, Brazil",
+    activities: [
+      "Visit the Christ the Redeemer statue",
+      "Relax at Copacabana Beach",
+      "Explore the Sugarloaf Mountain",
+      "Discover the Maracanã Stadium",
+      "Wander through the Santa Teresa neighborhood",
+    ],
+  },
+  {
+    name: "Dubai, UAE",
+    activities: [
+      "Visit the Burj Khalifa",
+      "Explore the Dubai Mall",
+      "Relax at Jumeirah Beach",
+      "Discover the Dubai Desert",
+      "See the Palm Jumeirah",
+    ],
+  },
+  {
+    name: "Amsterdam, Netherlands",
+    activities: [
+      "Visit the Rijksmuseum",
+      "Explore the Anne Frank House",
+      "Take a canal cruise",
+      "Wander through Vondelpark",
+      "See the Van Gogh Museum",
+    ],
+  },
+  {
+    name: "Lisbon, Portugal",
+    activities: [
+      "Explore the Belém Tower",
+      "Ride the Tram 28",
+      "Visit the Jerónimos Monastery",
+      "Stroll through Alfama District",
+      "Relax at the Lisbon Oceanarium",
+    ],
+  },
+  {
+    name: "Vienna, Austria",
+    activities: [
+      "Visit Schönbrunn Palace",
+      "Explore the Hofburg Palace",
+      "Stroll through Stadtpark",
+      "Enjoy a concert at the Vienna State Opera",
+      "See the Kunsthistorisches Museum",
+    ],
+  },
+
+  {
+    name: "Seoul, South Korea",
+    activities: [
+      "Visit Gyeongbokgung Palace",
+      "Explore Bukchon Hanok Village",
+      "Shop in Myeongdong",
+      "Stroll through Namsan Park",
+      "See the Dongdaemun Design Plaza",
+    ],
+  },
+  {
+    name: "Beijing, China",
+    activities: [
+      "Visit the Great Wall of China",
+      "Explore the Forbidden City",
+      "Stroll through Tiananmen Square",
+      "See the Summer Palace",
+      "Wander through the Temple of Heaven",
+    ],
+  },
+  {
+    name: "Bali, Indonesia",
+    activities: [
+      "Relax on Kuta Beach",
+      "Visit Uluwatu Temple",
+      "Explore the Sacred Monkey Forest",
+      "Stroll through rice terraces in Ubud",
+      "Enjoy a sunset at Tanah Lot",
+    ],
+  },
+  {
+    name: "Singapore",
+    activities: [
+      "Visit Gardens by the Bay",
+      "Explore Sentosa Island",
+      "Stroll through Orchard Road",
+      "See the Marina Bay Sands",
+      "Wander around Chinatown",
+    ],
+  },
+  {
+    name: "Hong Kong",
+    activities: [
+      "Take the Peak Tram to Victoria Peak",
+      "Explore Hong Kong Disneyland",
+      "Stroll through Tsim Sha Tsui",
+      "Visit the Big Buddha",
+      "Enjoy shopping at Mong Kok",
+    ],
+  },
+
+  {
+    name: "San Francisco, USA",
+    activities: [
+      "Visit the Golden Gate Bridge",
+      "Explore Alcatraz Island",
+      "Stroll through Fisherman's Wharf",
+      "See the Painted Ladies",
+      "Enjoy a ride on a cable car",
+    ],
+  },
+  {
+    name: "Los Angeles, USA",
+    activities: [
+      "Visit Hollywood Walk of Fame",
+      "Explore the Getty Center",
+      "Relax at Santa Monica Beach",
+      "Stroll through Griffith Park",
+      "See the Hollywood Sign",
+    ],
+  },
+  {
+    name: "Toronto, Canada",
+    activities: [
+      "Visit the CN Tower",
+      "Explore the Royal Ontario Museum",
+      "Stroll through Distillery District",
+      "Relax at Toronto Islands",
+      "See the Art Gallery of Ontario",
+    ],
+  },
+  {
+    name: "Vancouver, Canada",
+    activities: [
+      "Visit Stanley Park",
+      "Explore Granville Island",
+      "Stroll through Vancouver Aquarium",
+      "See Capilano Suspension Bridge",
+      "Relax at English Bay",
+    ],
+  },
+  {
+    name: "Buenos Aires, Argentina",
+    activities: [
+      "Explore La Boca neighborhood",
+      "Visit Recoleta Cemetery",
+      "Stroll through Palermo Parks",
+      "See the Casa Rosada",
+      "Enjoy tango dancing in San Telmo",
+    ],
+  },
+
+  {
+    name: "Marrakech, Morocco",
+    activities: [
+      "Visit the Jardin Majorelle",
+      "Explore the Medina",
+      "Stroll through Jemaa el-Fnaa Square",
+      "See the Koutoubia Mosque",
+      "Discover the Saadian Tombs",
+    ],
+  },
+  {
+    name: "Nairobi, Kenya",
+    activities: [
+      "Visit Nairobi National Park",
+      "Explore the Giraffe Centre",
+      "Stroll through Karen Blixen Museum",
+      "See the David Sheldrick Wildlife Trust",
+      "Discover Nairobi Railway Museum",
+    ],
+  },
+  {
+    name: "Cairo, Egypt",
+    activities: [
+      "Visit the Pyramids of Giza",
+      "Explore the Egyptian Museum",
+      "Stroll through Khan el-Khalili Bazaar",
+      "See the Sphinx",
+      "Wander through Islamic Cairo",
+    ],
+  },
+
+  {
+    name: "Melbourne, Australia",
+    activities: [
+      "Visit Federation Square",
+      "Explore the Royal Botanic Gardens",
+      "Stroll along Southbank",
+      "See the National Gallery of Victoria",
+      "Discover the Melbourne Zoo",
+    ],
+  },
+  {
+    name: "Auckland, New Zealand",
+    activities: [
+      "Visit Sky Tower",
+      "Explore Auckland Domain",
+      "Stroll through Viaduct Harbour",
+      "See the Auckland War Memorial Museum",
+      "Relax at Waiheke Island",
+    ],
+  },
+  {
+    name: "Helsinki, Finland",
+    activities: [
+      "Visit the Helsinki Cathedral",
+      "Explore the Market Square",
+      "Stroll through Esplanade Park",
+      "See the Suomenlinna Fortress",
+      "Enjoy the views from the Sky Park",
+    ],
+  },
+  {
+    name: "Stockholm, Sweden",
+    activities: [
+      "Visit the Vasa Museum",
+      "Explore Gamla Stan (Old Town)",
+      "Stroll through Skansen Open-Air Museum",
+      "See the Royal Palace",
+      "Relax at Djurgården Island",
+    ],
+  },
+  {
+    name: "Oslo, Norway",
+    activities: [
+      "Visit the Viking Ship Museum",
+      "Explore the Oslo Opera House",
+      "Stroll through Frogner Park",
+      "See the Munch Museum",
+      "Relax at Bygdøy Peninsula",
+    ],
+  },
+  {
+    name: "Copenhagen, Denmark",
+    activities: [
+      "Visit Tivoli Gardens",
+      "Explore Nyhavn Harbor",
+      "Stroll through Rosenborg Castle",
+      "See the Little Mermaid statue",
+      "Relax at Christianshavn",
+    ],
+  },
+  {
+    name: "Reykjavik, Iceland",
+    activities: [
+      "Visit Hallgrímskirkja Church",
+      "Explore the Golden Circle",
+      "Stroll through Harpa Concert Hall",
+      "See the Blue Lagoon",
+      "Discover Thingvellir National Park",
+    ],
+  },
+  {
+    name: "Aarhus, Denmark",
+    activities: [
+      "Visit ARoS Aarhus Art Museum",
+      "Explore Den Gamle By (The Old Town)",
+      "Stroll through the Botanical Gardens",
+      "See the Moesgaard Museum",
+      "Relax at the Tivoli Friheden amusement park",
+    ],
+  },
+  {
+    name: "Bergen, Norway",
+    activities: [
+      "Take the Fløibanen funicular to Mount Fløyen",
+      "Explore the Bryggen Wharf",
+      "Stroll through the Fish Market",
+      "Visit the Bergen Art Museum",
+      "Discover the Edvard Grieg Museum",
+    ],
+  },
+  {
+    name: "Gothenburg, Sweden",
+    activities: [
+      "Visit Liseberg Amusement Park",
+      "Explore Universeum Science Center",
+      "Stroll through Haga District",
+      "See the Gothenburg Art Museum",
+      "Relax at Slottsskogen Park",
+    ],
+  },
+  {
+    name: "Reykjanes Peninsula, Iceland",
+    activities: [
+      "Visit the Blue Lagoon",
+      "Explore the Bridge Between Continents",
+      "Stroll through the Reykjanesviti Lighthouse area",
+      "See the geothermal areas of Krýsuvík",
+      "Discover the Gunnuhver Hot Springs",
+    ],
+  },
+  {
+    name: "Torshavn, Faroe Islands",
+    activities: [
+      "Visit the Torshavn Cathedral",
+      "Explore the Old Town (Tinganes)",
+      "Stroll through the Nordic House",
+      "See the National Museum of the Faroe Islands",
+      "Discover the Tinganes Peninsula",
+    ],
+  },
+  {
+    name: "Warsaw, Poland",
+    activities: [
+      "Visit the Royal Castle",
+      "Explore Old Town Market Square",
+      "Walk through Łazienki Park",
+      "See the Palace of Culture and Science",
+      "Stroll along the Vistula Boulevards",
+    ],
+  },
+  {
+    name: "Budapest, Hungary",
+    activities: [
+      "Explore Buda Castle",
+      "Relax in Széchenyi Thermal Bath",
+      "Walk along the Danube Promenade",
+      "Visit Fisherman's Bastion",
+      "Tour the Hungarian Parliament Building",
+    ],
+  },
+  {
+    name: "Bratislava, Slovakia",
+    activities: [
+      "Visit Bratislava Castle",
+      "Explore Old Town",
+      "Walk along the Danube River",
+      "See St. Martin's Cathedral",
+      "Enjoy the view from UFO Bridge",
+    ],
+  },
+  {
+    name: "Ljubljana, Slovenia",
+    activities: [
+      "Stroll through Ljubljana Castle",
+      "Walk along the Ljubljanica River",
+      "Visit Tivoli Park",
+      "Explore the Central Market",
+      "See the Triple Bridge",
+    ],
+  },
+  {
+    name: "Zagreb, Croatia",
+    activities: [
+      "Visit Zagreb Cathedral",
+      "Explore the Upper Town",
+      "Relax in Maksimir Park",
+      "See the Museum of Broken Relationships",
+      "Walk through Ban Jelačić Square",
+    ],
+  },
+  {
+    name: "Belgrade, Serbia",
+    activities: [
+      "Walk through Kalemegdan Fortress",
+      "Explore Knez Mihailova Street",
+      "Visit the Church of Saint Sava",
+      "Relax at Ada Ciganlija",
+      "Discover the Belgrade Zoo",
+    ],
+  },
+  {
+    name: "Bucharest, Romania",
+    activities: [
+      "Tour the Palace of the Parliament",
+      "Explore the Old Town",
+      "Relax in Herăstrău Park",
+      "Visit the National Museum of Romanian History",
+      "Walk along Calea Victoriei",
+    ],
+  },
+  {
+    name: "Sofia, Bulgaria",
+    activities: [
+      "Visit Alexander Nevsky Cathedral",
+      "Explore Vitosha Boulevard",
+      "See the National Historical Museum",
+      "Relax in Borisova Gradina Park",
+      "Walk around the Boyana Church",
+    ],
+  },
+  {
+    name: "Chișinău, Moldova",
+    activities: [
+      "Visit Stefan Cel Mare Park",
+      "Explore the National Museum of History",
+      "Walk around the Central Market",
+      "See the Triumphal Arch",
+      "Relax at the Valea Morilor Park",
+    ],
+  },
+  {
+    name: "Kraków, Poland",
+    activities: [
+      "Visit Wawel Castle",
+      "Explore the Main Market Square",
+      "Walk through the Kazimierz District",
+      "See the St. Mary's Basilica",
+      "Relax in the Planty Park",
+    ],
+  },
+  {
+    name: "Gdańsk, Poland",
+    activities: [
+      "Visit the Gdańsk Old Town",
+      "Explore the Maritime Museum",
+      "Walk along the Long Market",
+      "See the Gdańsk Crane",
+      "Relax at the Oliwa Park",
+    ],
+  },
+  {
+    name: "Wrocław, Poland",
+    activities: [
+      "Visit the Wrocław Market Square",
+      "Explore the Ostrów Tumski",
+      "Walk across the Tumski Bridge",
+      "See the Wrocław Cathedral",
+      "Relax at the Słodowa Island",
+    ],
+  },
+  {
+    name: "Poznań, Poland",
+    activities: [
+      "Visit the Poznań Town Hall",
+      "Explore the Old Market Square",
+      "Walk through the Malta Lake",
+      "See the Imperial Castle",
+      "Relax in the Citadel Park",
+    ],
+  },
+];
